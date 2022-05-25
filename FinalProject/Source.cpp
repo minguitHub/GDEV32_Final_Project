9832f5841c1281ff603a2aedafc86eb49e335d5f
minguitHub <suminguitsylvane@gmail.com>
2022-05-25 20:01:39 +0800

    Adjusted light source direction to follow skybox's sun

--- FinalProject/Source.cpp
+++ FinalProject/Source.cpp
@@ -334,7 +334,7 @@ int main()
 	
 	// directional light uniforms
 		// directional light position
-		glm::vec3 directionalLightPos(5.0f, 8.0f, 2.0f);
+		glm::vec3 directionalLightPos(-4.5f, 8.0f, -5.5f);
 		int directionalLightPosLoc = glGetUniformLocation(ourShader.ID, "directionalLightPos");
 		glUniform3fv(directionalLightPosLoc, 1, glm::value_ptr(directionalLightPos));
 
@@ -424,7 +424,7 @@ int main()
 						// cube 2
 						model = glm::mat4(1.0f); // initialize identity matrix
 
-						model = glm::translate(model, glm::vec3(-0.25f, 0.25f, -0.5f));
+						model = glm::translate(model, glm::vec3(-3.0f, 0.25f, 1.0f));
 						model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 						model = glm::rotate(model, glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 						model = glm::scale(model, glm::vec3(0.50f, 0.50f, 0.50f));
@@ -509,7 +509,7 @@ int main()
 		// cube 2
 		model = glm::mat4(1.0f); // initialize identity matrix
 
-		model = glm::translate(model, glm::vec3(-0.25f, 0.25f, -0.5f));
+		model = glm::translate(model, glm::vec3(-3.0f, 0.25f, 1.0f));
 		model = glm::rotate(model, glm::radians(50.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 		model = glm::rotate(model, glm::radians(180.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 		model = glm::scale(model, glm::vec3(0.50f, 0.50f, 0.50f));
@@ -582,7 +582,7 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 // for input control in glfw
 void processInput(GLFWwindow* window)
 {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
 
 	const float cameraSpeed = 3 * deltaTime;
 	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) cameraPos += cameraSpeed * cameraFront;
